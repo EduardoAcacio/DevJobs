2563ce4b042fd0bb2acd004c2646cf5ccbdbee3c
Eduardo Acácio <ccarneiro@cpqi.com>
2018-06-29 11:16:50 -0300

    adicionando primeira pagina

---
+++ DevJobsCE.Web.UI/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace DevJobsCE.Web.UI.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
