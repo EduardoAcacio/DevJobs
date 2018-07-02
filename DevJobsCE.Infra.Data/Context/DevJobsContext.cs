a7f768840877358e94fbd14646099bf654c51851
Eduardo Acácio <ccarneiro@cpqi.com>
2018-07-02 16:25:20 -0300

    adding infra.data.context

---
+++ DevJobsCE.Infra.Data/Context/DevJobsContext.cs
@@ -0,0 +1,51 @@
+﻿using DevJobsCE.Domain.Entities;
+using System;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+
+namespace DevJobsCE.Infra.Data.Context
+{
+    public class DevJobsContext : DbContext
+    {
+        public DevJobsContext()
+            : base("DefaultConnection") { }
+
+        public DbSet<Job> Jobs { get; set; }
+        public DbSet<Company> Companies { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            modelBuilder.Conventions.Remove<OneToManyCascadeDeleteConvention>();
+            modelBuilder.Conventions.Remove<ManyToManyCascadeDeleteConvention>();
+
+            modelBuilder.Properties()
+               .Where(p => p.Name == p.ReflectedType.Name + "Id")
+               .Configure(p => p.IsKey());
+
+            modelBuilder.Properties<string>()
+                .Configure(p => p.HasColumnType("varchar"));
+
+            modelBuilder.Properties<string>()
+                .Configure(p => p.HasMaxLength(100));
+        }
+
+        public override int SaveChanges()
+        {
+            foreach (var entry in ChangeTracker.Entries().Where(entry => entry.Entity.GetType().GetProperty("InsertionDate") != null))
+            {
+                if (entry.State == EntityState.Added)
+                {
+                    entry.Property("InsertionDate").CurrentValue = DateTime.Now;
+                }
+
+                if (entry.State == EntityState.Modified)
+                {
+                    entry.Property("InsertionDate").IsModified = false;
+                }
+            }
+            return base.SaveChanges();
+        }
+    }
+}
